3ffbcfc92c17f324dc74e63d08d31718bb70668a
Supendi <irpan.supendi@gmail.com>
2024-04-10 08:46:40 +0700

    add unit test: IndexedErrorOf

---
+++ src/__tests__/types_tests/IndexedErrorOf.spec.ts
@@ -0,0 +1,29 @@
+import { IndexedErrorOf } from "../../types";
+
+
+/**
+ * Ensure all the code below are compiled
+ */
+
+describe("IndexedErrorOf Test", () => {
+    it("Should compile", () => {
+        interface Person {
+            name: string
+            age: number
+        }
+
+        let error: IndexedErrorOf<Person> = {
+            index: 0,
+            errors: {
+                age: ["Required"],
+                name: ["Required"]
+            },
+            validatedObject: {
+                name: "",
+                age: 0
+            }
+        }
+
+        expect(error).not.toBeNull()
+    })
+})
