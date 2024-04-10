c1b56df4298ffb936039a9a8ed713b87494d6ab8
Supendi <irpan.supendi@gmail.com>
2024-04-10 08:36:19 +0700

    add unit test: PossiblyUndefined

---
+++ src/__tests__/types_tests/PossiblyUndefined.spec.ts
@@ -0,0 +1,27 @@
+import { PossiblyUndefined } from "../../types";
+
+
+/**
+ * Ensure all the code below are compiled
+ */
+
+describe("PossiblyUndefined Test", () => {
+    it("Should compile", () => {
+        interface Person {
+            name: PossiblyUndefined<string>,
+            age: PossiblyUndefined<number>
+        }
+
+        let person: Person = {
+            name: undefined, 
+            age: undefined,
+        }
+
+        person = {
+            name: "John",
+            age: 1,
+        }
+
+        expect(person).not.toBeNull()
+    })
+})
