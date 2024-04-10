6260a6cc62c6c078f0fa0672c840b18c8891ee01
Supendi <irpan.supendi@gmail.com>
2024-04-10 07:36:06 +0700

    add unit test: possibly undefined property

--- src/__tests__/types_tests/types.ArrayValidationRule.spec.ts
+++ src/__tests__/types_tests/types.ArrayValidationRule.spec.ts
@@ -1,44 +1,71 @@
-import { PropertyValidator, ValidationRule, ArrayValidationRule } from "../../types"
+import { ValidationRule, ArrayValidationRule, PropertyValidator } from "../../types" 
+
+const requiredValidator: PropertyValidator<any, any> = {
+    description: "Required Validator",
+    returningErrorMessage: "This field is required",
+    validate: (value: any, obj?: any) => {
+        return !!value
+    }
+}
+const minNumberValidator: PropertyValidator<any, any> = {
+    description: "Minimum Number Validator",
+    returningErrorMessage: "Minimum number is",
+    validate: (value: any, obj?: any) => {
+        return false
+    }
+}
 
 /**
  * Ensure all the code below compiled
  */
-
-describe("ArrayValidationRule Simple Person Test", () => {
+describe("ArrayValidationRule Compile Test", () => {
     it("Should compile", () => {
         interface Person {
             name: string
             age: number
         }
-        const requiredValidator: PropertyValidator<any, any> = {
-            description: "Required Validator",
-            returningErrorMessage: "This field is required",
-            validate: (value: any, obj?: any) => {
-                return !!value
-            }
-        }
-        const minNumberValidator: PropertyValidator<any, any> = {
-            description: "Minimum Number Validator",
-            returningErrorMessage: "Minimum number is",
-            validate: (value: any, obj?: any) => {
-                return false
-            }
-        }
+        
         const personRule: ValidationRule<Person> = {
             name: [requiredValidator],
             age: [requiredValidator, minNumberValidator],
         }
 
-        const arrayOfPersonRule: ArrayValidationRule<Person, Person[]> = {
+        const arrayOfPersonValidationRule: ArrayValidationRule<Person, Person[]> = {
             validators: [requiredValidator],
             validationRule: personRule,
         }
 
-        expect(arrayOfPersonRule).not.toBeUndefined()
+        expect(arrayOfPersonValidationRule).not.toBeUndefined()
 
-        expect(arrayOfPersonRule.validators).not.toBeUndefined()
-        expect(Array.isArray(arrayOfPersonRule.validators)).toBeTruthy()
-        expect(arrayOfPersonRule.validators?.length).toEqual(1)
-        expect(arrayOfPersonRule.validators ? arrayOfPersonRule.validators[0] : undefined).toEqual(requiredValidator)
+        expect(arrayOfPersonValidationRule.validators).not.toBeUndefined()
+        expect(Array.isArray(arrayOfPersonValidationRule.validators)).toBeTruthy()
+        expect(arrayOfPersonValidationRule.validators?.length).toEqual(1)
+        expect(arrayOfPersonValidationRule.validators ? arrayOfPersonValidationRule.validators[0] : undefined).toEqual(requiredValidator)
+    })
+})
+
+describe("ArrayValidationRule Compile Test", () => {
+    it("Should compile", () => {
+        interface Person {
+            name?: string
+            age?: number
+        }
+
+        const personRule: ValidationRule<Person> = {
+            name: [requiredValidator],
+            age: [requiredValidator, minNumberValidator],
+        }
+
+        const arrayOfPersonValidationRule: ArrayValidationRule<Person, Person[]> = {
+            validators: [requiredValidator],
+            validationRule: personRule,
+        }
+
+        expect(arrayOfPersonValidationRule).not.toBeUndefined()
+
+        expect(arrayOfPersonValidationRule.validators).not.toBeUndefined()
+        expect(Array.isArray(arrayOfPersonValidationRule.validators)).toBeTruthy()
+        expect(arrayOfPersonValidationRule.validators?.length).toEqual(1)
+        expect(arrayOfPersonValidationRule.validators ? arrayOfPersonValidationRule.validators[0] : undefined).toEqual(requiredValidator)
     })
 })
