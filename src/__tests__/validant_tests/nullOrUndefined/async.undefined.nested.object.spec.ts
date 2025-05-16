730a4f668e1e95bbebb634d32be068a6190317a2
Supendi <irpan.supendi@gmail.com>
2025-05-16 18:25:08 +0700

    siap

---
+++ src/__tests__/validant_tests/nullOrUndefined/async.undefined.nested.object.spec.ts
@@ -0,0 +1,46 @@
+import { AsyncValidationRule } from "../../../types/AsyncValidationRule"
+import validant, { ValidationResult } from "../../../validant"
+
+interface Person {
+    name: string
+    age: 1
+    children?: Person
+}
+
+const rule: AsyncValidationRule<Person> = {
+    name: [undefined],
+    age: undefined,
+    children: undefined
+}
+const person: Person = {
+    name: "",
+    age: 1,
+}
+
+describe("Test Validate Against Undefined", () => {
+    it("throw error", async () => {
+        const actual = await validant.validateAsync(person, rule)
+
+        const expected: ValidationResult<Person> = {
+            message: "Good to go.",
+            isValid: true,
+            errors: undefined
+        }
+
+        expect(actual).toEqual(expected)
+    })
+})
+
+describe("Test Validate Against null", () => {
+    it("throw error", async () => {
+        const actual = await validant.validateAsync(person, rule)
+
+        const expected: ValidationResult<Person> = {
+            message: "Good to go.",
+            isValid: true,
+            errors: undefined
+        }
+
+        expect(actual).toEqual(expected)
+    })
+})
